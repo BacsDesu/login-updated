e613d63f9aa0c68f989a025b3c83080d49de4650
BacsDesu <bacalingmarkjefferson@gmail.com>
2026-03-04 10:13:52 +0800

    Delete login directory

--- login/reset_password.php
+++
@@ -1,6 +0,0 @@
-<?php
-// This file is kept for backward compatibility
-// All password reset functionality is now handled in forgot_password.php
-header('Location: forgot_password.php');
-exit;
-?>
